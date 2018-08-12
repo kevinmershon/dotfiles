e2e9782a6f30ff13a2a9143ec75c3225b427b9cd
Kevin Mershon <nwlinkvxd@gmail.com>
2018-08-11 21:29:03 -0700

    Dotfiles updates

--- tmux/tmux-work.sh
+++ tmux/tmux-work.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
+. ~/.bashrc
 eval tmux has-session -t work > /dev/null 2>&1
 if [ $? != 1 ]; then
     tmux -2 attach -t work
     exit
 fi
 cd ~/workspace
+login # from bashrc
 tmux new-session -d -s work
+tmux set -g base-index 1
 
 kernel=`uname`
 if [ "$kernel" == "Linux" ]; then
     tmux new-window -t work:1 -n 'Vim' "gvim -v"
 elif [ "$kernel" == "Darwin" ]; then
-    tmux new-window -t work:1 -n 'Vim' "mvim -v"
+    tmux new-window -t work:1 -n 'Vim' "vim"
 fi
 tmux new-window -t work:2 -n 'Bash' bash
-tmux new-window -t work:3 -n 'IRC' irssi
 tmux kill-window -t work:0
 
-tmux join-pane -h -s work:2 -t work:1
-tmux join-pane -v -s work:3 -t work:1
+tmux join-pane -v -p 20 -s work:2 -t work:1
 tmux select-window -t work:1
+tmux select-pane -t 0
 tmux -2 attach-session -t work
