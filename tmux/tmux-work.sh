458df65192095eea4a2b9eaa89d5c7ee8a71fb0a
Kevin Mershon <kevin.mershon@scscloud.com>
2019-05-23 11:57:20 -0700

    New tmux-layout with console at top

--- tmux/tmux-work.sh
+++ tmux/tmux-work.sh
@@ -9,17 +9,17 @@ cd ~/workspace
 login # from bashrc
 tmux new-session -d -s work
 tmux set -g base-index 1
+tmux new-window -t work:1 -n 'Bash' bash
+tmux kill-window -t work:0
 
 kernel=`uname`
 if [ "$kernel" == "Linux" ]; then
-    tmux new-window -t work:1 -n 'Vim' "gvim -v"
+    tmux new-window -t work:2 -n 'Vim' "gvim -v"
 elif [ "$kernel" == "Darwin" ]; then
-    tmux new-window -t work:1 -n 'Vim' "vim"
+    tmux new-window -t work:2 -n 'Vim' "vim"
 fi
-tmux new-window -t work:2 -n 'Bash' bash
-tmux kill-window -t work:0
 
-tmux join-pane -v -p 20 -s work:2 -t work:1
+tmux join-pane -v -s work:2 -t work:1
 tmux select-window -t work:1
-tmux select-pane -t 0
-tmux -2 attach-session -t work
+tmux select-pane -t 1
+tmux -2 attach-session -t work \; resize-pane -t 0 -y 12
